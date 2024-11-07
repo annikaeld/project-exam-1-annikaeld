b28ba49f7b2329bf9cc9590d43df6b7f4fa84a36
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-11-07 12:37:37 +0100

    footer

--- js/apiContent/fetchPost.js
+++ js/apiContent/fetchPost.js
@@ -1,88 +1,106 @@
-import { API_BASE_URL, fetchContent } from './fetchContent.js';
-import { getQueryParam } from '../getQueryParam.js';
+import { API_BASE_URL, fetchContent } from "./fetchContent.js";
+import { getQueryParam } from "../getQueryParam.js";
 
 async function fetchPost(id) {
-    const url = `${API_BASE_URL}/posts/${id}`;
-    await fetchContent(url);
+  const url = `${API_BASE_URL}/posts/${id}`;
+  await fetchContent(url);
 }
 
 async function fetchPostFromUrl() {
-    const id = getQueryParam('id');
-    if (id) {
-        await fetchPost(id);
-    } else {
-        document.getElementById('content').innerHTML = 'No id provided in URL.';
-    }
+  const id = getQueryParam("id");
+  if (id) {
+    await fetchPost(id);
+  } else {
+    document.getElementById("content").innerHTML = "No id provided in URL.";
+  }
 }
 
 async function fetchLatestPosts(limit = 9) {
-    const url = `${API_BASE_URL}/posts?per_page=${limit}&_embed`;
-    const response = await fetch(url);
-    const posts = await response.json();
-    console.log('Latest posts:', posts);
-    return posts;
+  const url = `${API_BASE_URL}/posts?per_page=${limit}&_embed`;
+  const response = await fetch(url);
+  const posts = await response.json();
+  console.log("Latest posts:", posts);
+  return posts;
 }
 
 function extractFirstImage(content) {
-    const imgTagMatch = content.match(/<img[^>]+src="([^">]+)"/);
-    return imgTagMatch ? imgTagMatch[1] : '';
+  const imgTagMatch = content.match(/<img[^>]+src="([^">]+)"/);
+  return imgTagMatch ? imgTagMatch[1] : "";
 }
 
 async function displayLatestPosts(numberOfPosts) {
-    try {
-        const posts = await fetchLatestPosts(numberOfPosts);
-        const contentElement = document.getElementById('blogposts');
-        contentElement.innerHTML = '';
+  try {
+    const posts = await fetchLatestPosts(numberOfPosts);
+    const contentElement = document.getElementById("blogposts");
+    contentElement.innerHTML = "";
 
-        posts.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'post';
+    posts.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.className = "post";
 
-            // Get the featured image URL or the first image from the content
-            const featuredImage = post._embedded && post._embedded['wp:featuredmedia'] ? post._embedded['wp:featuredmedia'][0].source_url : extractFirstImage(post.content.rendered);
-            console.log('Featured image:', featuredImage);
+      // Get the featured image URL or the first image from the content
+      const featuredImage =
+        post._embedded && post._embedded["wp:featuredmedia"]
+          ? post._embedded["wp:featuredmedia"][0].source_url
+          : extractFirstImage(post.content.rendered);
+      console.log("Featured image:", featuredImage);
 
-            postElement.innerHTML = `
+      postElement.innerHTML = `
                 <h2>${post.title.rendered}</h2>
-                ${featuredImage ? `<img src="${featuredImage}" alt="${post.title.rendered}">` : ''}
-                <p>${post.excerpt.rendered}</p>
+                ${
+                  featuredImage
+                    ? `<img src="${featuredImage}" alt="${post.title.rendered}">`
+                    : ""
+                }
                 <a href="api-post.html?id=${post.id}">Les meir</a>
             `;
-            contentElement.appendChild(postElement);
-        });
-    } catch (error) {
-        console.error('Error fetching latest posts:', error);
-    }
+      contentElement.appendChild(postElement);
+    });
+  } catch (error) {
+    console.error("Error fetching latest posts:", error);
+  }
 }
 
 async function postsForCarousel(numberOfPosts) {
-    try {
-        const posts = await fetchLatestPosts(numberOfPosts);
-        const contentElement = document.getElementById('carousel-content');
-        contentElement.innerHTML = '';
-        var i= 0;
+  try {
+    const posts = await fetchLatestPosts(numberOfPosts);
+    const contentElement = document.getElementById("carousel-content");
+    contentElement.innerHTML = "";
+    var i = 0;
 
-        posts.forEach(post => {
-            const postElement = document.createElement('div');
-            postElement.className = 'card-container';
+    posts.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.className = "card-container";
 
-            // Get the featured image URL or the first image from the content
-            const featuredImage = post._embedded && post._embedded['wp:featuredmedia'] ? post._embedded['wp:featuredmedia'][0].source_url : extractFirstImage(post.content.rendered);
-            console.log('Featured image:', featuredImage);
+      // Get the featured image URL or the first image from the content
+      const featuredImage =
+        post._embedded && post._embedded["wp:featuredmedia"]
+          ? post._embedded["wp:featuredmedia"][0].source_url
+          : extractFirstImage(post.content.rendered);
+      console.log("Featured image:", featuredImage);
 
-            i++;
+      i++;
 
-            postElement.innerHTML = `
+      postElement.innerHTML = `
                 <div class="card card${i}">
-                ${featuredImage ? `<img src="${featuredImage}" alt="${post.title.rendered}">` : ''}
+                ${
+                  featuredImage
+                    ? `<img src="${featuredImage}" alt="${post.title.rendered}">`
+                    : ""
+                }
                 <h2>${post.title.rendered}</h2>
                 </div>
             `;
-            contentElement.appendChild(postElement);
-        });
-    } catch (error) {
-        console.error('Error fetching latest posts:', error);
-    }
+      contentElement.appendChild(postElement);
+    });
+  } catch (error) {
+    console.error("Error fetching latest posts:", error);
+  }
 }
 
-export { fetchPostFromUrl, fetchLatestPosts, displayLatestPosts, postsForCarousel };
+export {
+  fetchPostFromUrl,
+  fetchLatestPosts,
+  displayLatestPosts,
+  postsForCarousel,
+};
