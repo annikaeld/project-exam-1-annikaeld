68cb8b47556702662f7f1794ca9c699834067fe3
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-11-10 22:07:21 +0100

    error messages in form

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,24 @@
-import { validateEmail } from "./ui/formValidation.js";
+import { validateEmail } from "./ui/formValidation.js"
 import { checkLength } from "./ui/formValidation.js";
 
 const form = document.querySelector("#contact");
 const name = document.querySelector("#name");
-const name-error = document.querySelector("#name-error");
+const nameError = document.querySelector("#name-error");
 const email = document.querySelector("#email");
-const email-error = document.querySelector("#email-error");
+const emailError = document.querySelector("#email-error");
 const subject = document.querySelector("#subject");
-const subject-error = document.querySelector("#subject-error");
+const subjectError = document.querySelector("#subject-error");
 const message = document.querySelector("#message");
-const messageError = document.querySelector("#messageError");
+const messageError = document.querySelector("#message-error");
 
 function validateForm(event) {
   let isValid = true;
+  console.log("validateForm");
 
   if (checkLength(name.value, 5) === true) {
-    messageError.style.display = "none";
+    nameError.style.display = "none";
   } else {
-    messageError.style.display = "block";
+    nameError.style.display = "block";
     isValid = false;
   }
 
@@ -29,9 +30,9 @@ function validateForm(event) {
   }
 
   if (checkLength(subject.value, 15) === true) {
-    messageError.style.display = "none";
+    subjectError.style.display = "none";
   } else {
-    messageError.style.display = "block";
+    subjectError.style.display = "block";
     isValid = false;
   }
 
@@ -42,8 +43,10 @@ function validateForm(event) {
     isValid = false;
   }
 
-  // If the form is not valid, prevent it from being submitted
-  if (!isValid) {
-    event.preventDefault();
-  }
+  return isValid
 }
+
+export { validateForm };
+window.validateForm = validateForm;
+
+form.addEventListener("submit", validateForm);
