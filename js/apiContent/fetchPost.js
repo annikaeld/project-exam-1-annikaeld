74029414195a2d036c77de10e23517d93ec3ddbe
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-11-13 23:23:56 +0100

    Load more button. Footer with SoMe icons.

--- js/apiContent/fetchPost.js
+++ js/apiContent/fetchPost.js
@@ -1,5 +1,8 @@
 import { API_BASE_URL, fetchContent } from "./fetchContent.js";
 import { getQueryParam } from "../getQueryParam.js";
+let postPageNumber = 1;
+let numberOfPostsPerPage = 10;
+let totalPages = 1;
 
 async function fetchPost(id) {
   const url = `${API_BASE_URL}/posts/${id}`;
@@ -15,11 +18,29 @@ async function fetchPostFromUrl() {
   }
 }
 
-async function fetchLatestPosts(limit = 9) {
-  const url = `${API_BASE_URL}/posts?per_page=${limit}&_embed`;
+async function fetchLatestPosts() {
+  const url = `${API_BASE_URL}/posts?per_page=${numberOfPostsPerPage}&page=1&_embed`;
   const response = await fetch(url);
   const posts = await response.json();
+  postPageNumber++;
+  totalPages = parseInt(response.headers.get('X-WP-TotalPages'), 10);
   return posts;
+}
+
+async function fetchMorePosts() {
+  const url = `${API_BASE_URL}/posts?per_page=${numberOfPostsPerPage}&page=${postPageNumber}&_embed`;
+  const response = await fetch(url);
+  const posts = await response.json();
+  postPageNumber++;
+  const morePostsButton = document.getElementById("more-posts-button");
+  if (lastPostFetched()) {
+    morePostsButton.style.display = "none";
+  }
+  return posts;
+}
+
+function lastPostFetched() {
+  return postPageNumber > totalPages;
 }
 
 function extractFirstImage(content) {
@@ -36,40 +57,54 @@ function norwegianDate(date) {
   }).format(dateObj);
 }
 
-async function displayLatestPosts(numberOfPosts) {
+async function displayLatestPosts() {
   try {
-    const posts = await fetchLatestPosts(numberOfPosts);
+    const posts = await fetchLatestPosts();
     const contentElement = document.getElementById("blogposts");
     contentElement.innerHTML = "";
 
     posts.forEach((post) => {
-      const postElement = document.createElement("div");
-      postElement.className = "post";
-
-      // Get the featured image URL or the first image from the content
-      const featuredImage =
-        post._embedded && post._embedded["wp:featuredmedia"]
-          ? post._embedded["wp:featuredmedia"][0].source_url
-          : extractFirstImage(post.content.rendered);
-
-      // Format the date
-      const formattedDate = norwegianDate(post.date);
-
-      postElement.innerHTML = `
-                <a href="api-post.html?id=${post.id}">
-                ${
-                  featuredImage
-                    ? `<img src="${featuredImage}" alt="${post.title.rendered}">`
-                    : ""
-                }
-                <h2>${post.title.rendered}</h2>
-                <p class="date-blog">${formattedDate}</p>
-                </a>
-            `;
-      contentElement.appendChild(postElement);
+      contentElement.appendChild(postToElement(post));
     });
   } catch (error) {
     console.error("Error fetching latest posts:", error);
+  }
+}
+
+function postToElement(post) {
+  const postElement = document.createElement("div");
+  postElement.className = "post";
+
+  // Get the featured image URL or the first image from the content
+  const featuredImage =
+    post._embedded && post._embedded["wp:featuredmedia"]
+      ? post._embedded["wp:featuredmedia"][0].source_url
+      : extractFirstImage(post.content.rendered);
+
+  postElement.innerHTML = `
+            <a href="api-post.html?id=${post.id}">
+            ${
+              featuredImage
+                ? `<img src="${featuredImage}" alt="${post.title.rendered}">`
+                : ""
+            }
+            <h2>${post.title.rendered}</h2>
+            <p class="date-blog">${norwegianDate(post.date)}</p>
+            </a>
+        `;
+  return postElement;
+}
+
+async function displayMorePosts(numberOfPosts) {
+  try {
+    const posts = await fetchMorePosts()
+    const contentElement = document.getElementById("blogposts");
+    posts.forEach((post) => {
+      contentElement.appendChild(postToElement(post));
+    });
+  }
+  catch (error) {
+    console.error("Error fetching more posts:", error);
   }
 }
 
@@ -120,5 +155,6 @@ export {
   fetchPostFromUrl,
   fetchLatestPosts,
   displayLatestPosts,
+  displayMorePosts,
   postsForCarousel,
 };
--- js/footer.js
+++ js/footer.js
@@ -2,10 +2,16 @@ import { openingHours } from './openingHours.js';
 function addFooter() {
   var footerElement = document.querySelector('.footer');
   footerElement.innerHTML = `
-    <div class="footer-left">
+    <div class="footer-item" id="item-logo">
       <a href="index.html"> <img src="/pictures/STOKK logo kvit.svg" alt="logo of STOKK" class="bottom-logo"></a>
     </div>
-    <div class="footer-right">
+    <div class="footer-item some" id="item-facebook">
+      <a href="https://www.facebook.com/stokk.org"><img src="/pictures/facebook.svg" alt="facebook logo" class="social-media"></a>
+    </div>
+    <div class="footer-item some" id="item-instagram">
+      <a href="https://www.instagram.com/stordtindeogklatreklubb/"><img src="/pictures/instagram.svg" alt="instagram logo" class="social-media"></a>
+    </div>
+    <div class="footer-item" id="item-opening-hours">
       ${openingHours()}
     </div>
   `;
