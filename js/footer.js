ed6ff9db99e00eae6ff98a94eee3b3add1700db3
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-11-16 18:34:34 +0100

    html validation

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ function addFooter() {
   var footerElement = document.querySelector('.footer');
   footerElement.innerHTML = `
     <div class="footer-item" id="item-logo">
-      <a href="index.html"> <img src="/pictures/STOKK logo kvit.svg" alt="logo of STOKK" class="bottom-logo"></a>
+      <a href="index.html"> <img src="/pictures/STOKK-logo-kvit.svg" alt="logo of STOKK" class="bottom-logo"></a>
     </div>
     <div class="footer-item some" id="item-facebook">
       <a href="https://www.facebook.com/stokk.org"><img src="/pictures/facebook.svg" alt="facebook logo" class="social-media"></a>
