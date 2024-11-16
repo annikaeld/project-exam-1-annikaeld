b38eab933f252b4226a1cb45cc081a8ebd252d9e
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-11-16 09:49:38 +0100

    Don't display alt text as caption on modal image

--- js/modalImage.js
+++ js/modalImage.js
@@ -8,12 +8,10 @@ function setupModalImage() {
     var img = document.querySelector("#content img");
     console.log("Image:", img);
     var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("modal-caption");
 
     img.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
     }
 
     // Get the <span> element that closes the modal
