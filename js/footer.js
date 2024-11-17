25b0a021ff226cf1b08c80c8d88c093cca88c73e
annikaeld <130365506+annikaeld@users.noreply.github.com>
2024-11-17 15:14:30 +0100

    footer styling

--- js/footer.js
+++ js/footer.js
@@ -1,14 +1,12 @@
-import { openingHours } from './openingHours.js';
+import { openingHours } from "./openingHours.js";
 function addFooter() {
-  var footerElement = document.querySelector('.footer');
+  var footerElement = document.querySelector(".footer");
   footerElement.innerHTML = `
     <div class="footer-item" id="item-logo">
       <a href="index.html"> <img src="/pictures/stokk-logo-kvit.svg" alt="stokk logo" class="bottom-logo"></a>
     </div>
-    <div class="footer-item some" id="item-facebook">
+    <div class="footer-item some" id="item-social-media">
       <a href="https://www.facebook.com/stokk.org"><img src="/pictures/facebook.svg" alt="facebook logo" class="social-media"></a>
-    </div>
-    <div class="footer-item some" id="item-instagram">
       <a href="https://www.instagram.com/stordtindeogklatreklubb/"><img src="/pictures/instagram.svg" alt="instagram logo" class="social-media"></a>
     </div>
     <div class="footer-item" id="item-opening-hours">
